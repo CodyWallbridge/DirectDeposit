033f7bee04e05c9dd418912a93c8bf29b15c06d4
CodyWallbridge <79122609+CodyWallbridge@users.noreply.github.com>
2025-03-02 10:27:15 -0600

    V1.9.3 (#11)
    - fixed bug causing wishlist search functionality to reset items
    - fixed reagent bag not being included in available items to deposit
    - added cata classic items
    - added cata classic versioning

--- DirectDeposit.lua
+++ DirectDeposit.lua
@@ -337,6 +337,52 @@ function DirectDepositEventFrame:onLoad()
     debugPrint("done onLoad")
 end
 
+function DirectDepositEventFrame:DirectDepositRemoveOldItems()
+    local tradeGoods
+    local locale = GetLocale()
+
+    -- if there ends up being multiple classic clients, this link has all the enums for the different versions
+    -- https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+    IsClassic = WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE
+    if IsClassic then
+        locale = "CataClassic"
+    end
+    
+    function merge_tables(table1, table2)
+        local result = {}
+        local name_set = {}
+        
+        local function insert_into_result(t)
+            for id, name in pairs(t) do
+                if not name_set[name] then
+                    result[id] = name
+                    name_set[name] = true
+                end
+            end
+        end
+        insert_into_result(table1)
+        insert_into_result(table2)
+    
+        return result
+    end
+
+    tradeGoods = merge_tables(DirectDeposit_TRADE_GOODS[locale], DirectDeposit_CONSUMABLES[locale])
+
+    -- go through requestedItems and depositingItems and if the item does not exist in tradeGoods, remove it from the list
+    for i = #requestedItems, 1, -1 do
+        local found = false
+        for _, tradeItem in pairs(tradeGoods) do
+            if requestedItems[i].name == tradeItem then
+                found = true
+                break
+            end
+        end
+        if not found then
+            table.remove(requestedItems, i)
+        end
+    end
+end
+
 local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("BAG_UPDATE")
 -- button deposits everything from list, no handling of ranks whatsoever.
